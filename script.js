3b07297de0a9caa505a80c8a5ff737cdf2a2fedf
Diptesh Tarafdar <diptesh.tarafdar@gmail.com>
2022-06-23 17:33:08 +0530

    added searching of todos feaature

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const addForm = document.querySelector('.add');
 const list = document.querySelector('.todos');
 
+const search = document.querySelector('.search input');
+
 const generateTemplate = (todo) => {
     const html = `<li class="list-group-item text-light d-flex justify-content-between align-items-center">
     <span>${todo}</span>
@@ -25,3 +27,21 @@ list.addEventListener('click', (e) => {
         e.target.parentElement.remove();
     }
 });
+
+const filterTodos = (term) => {
+    Array.from(list.children)
+        .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.add('filtered'))
+
+        Array.from(list.children)
+        .filter((todo) => todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.remove('filtered'))
+};
+
+//keyup event
+search.addEventListener('keyup', ()=> {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
+});
+
+
